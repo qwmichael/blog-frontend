273dfa4e73e8b48f92bf8b4d43283449a99388ec
github_qwmichael <michaelng9413@gmail.com>
2020-01-26 20:44:04 +1100

    Updates

---
+++ precache-manifest.ff960a243b30941b241f1c01ff97a9c8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0b1bd27238adf49bbd19e6cca12e74e",
+    "url": "/blog-frontend/index.html"
+  },
+  {
+    "revision": "04546954075f3e9ad099",
+    "url": "/blog-frontend/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "e7aad39de48a12cfea7e",
+    "url": "/blog-frontend/static/css/main.d6a958cb.chunk.css"
+  },
+  {
+    "revision": "04546954075f3e9ad099",
+    "url": "/blog-frontend/static/js/2.d0a69847.chunk.js"
+  },
+  {
+    "revision": "ff6f1f3be7ff3c092017e2974c44a438",
+    "url": "/blog-frontend/static/js/2.d0a69847.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e7aad39de48a12cfea7e",
+    "url": "/blog-frontend/static/js/main.bf7792cd.chunk.js"
+  },
+  {
+    "revision": "95e7fe4964997b062a30",
+    "url": "/blog-frontend/static/js/runtime-main.6c9a5dde.js"
+  }
+]);
