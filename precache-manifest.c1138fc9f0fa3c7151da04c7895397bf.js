7012e8e06b85466901b888f3a15fdc7357357a5b
github_qwmichael <michaelng9413@gmail.com>
2020-01-26 21:23:28 +1100

    Updates

--- precache-manifest.c1138fc9f0fa3c7151da04c7895397bf.js
+++ precache-manifest.c1138fc9f0fa3c7151da04c7895397bf.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "438b3ebed32a06a7829079fbca7c1b63",
+    "revision": "471ee651e72bfa10bab907e996b78e2f",
     "url": "/blog-frontend/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/blog-frontend/static/css/2.17e5ed98.chunk.css"
   },
   {
-    "revision": "d3d969845de885ad3df4",
+    "revision": "21c435cf1cb8afb3e173",
     "url": "/blog-frontend/static/css/main.d6a958cb.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/blog-frontend/static/js/2.d0a69847.chunk.js.LICENSE"
   },
   {
-    "revision": "d3d969845de885ad3df4",
-    "url": "/blog-frontend/static/js/main.daf8dc7e.chunk.js"
+    "revision": "21c435cf1cb8afb3e173",
+    "url": "/blog-frontend/static/js/main.adb695c9.chunk.js"
   },
   {
     "revision": "95e7fe4964997b062a30",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/blog-frontend/precache-manifest.e2c0c81d1cc8434af0a1a246a797f920.js"
+  "/blog-frontend/precache-manifest.c1138fc9f0fa3c7151da04c7895397bf.js"
 );
 
 self.addEventListener('message', (event) => {
